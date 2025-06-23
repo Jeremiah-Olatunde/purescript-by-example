07d002576976f3e5733c2bb50f8e57f18cab8ffc
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-06-23 19:43:02 +0100

    feat(ch5): add solution count-even

--- exercises/chapter5/test/MySolutions.purs
+++ exercises/chapter5/test/MySolutions.purs
@@ -3,6 +3,8 @@ module Test.MySolutions where
 import Prelude
 
 import Data.Ord (abs)
+import Data.Array (head, tail)
+import Data.Maybe (fromMaybe)
 
 -- Note to reader: Add your solutions to this file
 
@@ -13,3 +15,11 @@ isEven = isEven' <<< abs
   isEven' 0 = true
   isEven' 1 = false
   isEven' n = isEven' (n - 2)
+
+countEven :: Array Int -> Int
+countEven [] = 0
+countEven ns = add increment $ countEven $ fromMaybe [] $ tail ns
+  where
+  oneIfEven n = if mod n 2 == 0 then 1 else 0
+  increment = fromMaybe 0 $ map oneIfEven $ (head ns)
+
