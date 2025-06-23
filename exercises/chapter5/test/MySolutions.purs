1187eaa20b3dc913e18f7666b2bdc1036b291a5b
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-06-23 19:20:41 +0100

    feat(ch5): add solution is-even

--- exercises/chapter5/test/MySolutions.purs
+++ exercises/chapter5/test/MySolutions.purs
@@ -2,4 +2,14 @@ module Test.MySolutions where
 
 import Prelude
 
+import Data.Ord (abs)
+
 -- Note to reader: Add your solutions to this file
+
+isEven :: Int -> Boolean
+isEven = isEven' <<< abs
+  where
+  isEven' :: Int -> Boolean
+  isEven' 0 = true
+  isEven' 1 = false
+  isEven' n = isEven' (n - 2)
