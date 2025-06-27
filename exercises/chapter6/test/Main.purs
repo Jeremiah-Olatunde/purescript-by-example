111189bdb778438414d46946a301e8c8a0b48684
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-06-27 12:10:32 +0100

    fix(ch6): include excluded test for non-empty-eq

--- exercises/chapter6/test/Main.purs
+++ exercises/chapter6/test/Main.purs
@@ -87,11 +87,12 @@ main =
           Assert.equal (NonEmpty 1 [ 2, 3 ])
             $ NonEmpty 1 [ 2, 3 ]
 
-{-  Move this block comment starting point to enable more tests
         test "not equals" do
           Assert.expectFailure "should not be equal"
             $ Assert.equal (NonEmpty 1 [ 2, 3 ])
             $ NonEmpty 2 [ 2, 3 ]
+
+{-  Move this block comment starting point to enable more tests
       suite "Semigroup NonEmpty" do
         test "append" do
           Assert.equal (NonEmpty 1 [ 2, 3, 4, 5, 6 ])
