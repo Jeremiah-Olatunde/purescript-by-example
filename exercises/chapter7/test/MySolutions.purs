e4f04729c90287089f97ae4fdefc61bfb9c3d471
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-06-29 18:07:50 +0100

    feat(ch7): add solution maybe-add-sub-mul-div

--- exercises/chapter7/test/MySolutions.purs
+++ exercises/chapter7/test/MySolutions.purs
@@ -1,5 +1,20 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.Maybe (Maybe)
+import Control.Apply (lift2)
 
 -- Note to reader: Add your solutions to this file
+
+addMaybe :: forall a. Semiring a => Maybe a -> Maybe a -> Maybe a
+addMaybe = lift2 (+)
+
+mulMaybe :: forall a. Semiring a => Maybe a -> Maybe a -> Maybe a
+mulMaybe = lift2 (*)
+
+subMaybe :: forall a. Ring a => Maybe a -> Maybe a -> Maybe a
+subMaybe = lift2 (-)
+
+divMaybe :: forall a. EuclideanRing a => Maybe a -> Maybe a -> Maybe a
+divMaybe = lift2 (/)
+
