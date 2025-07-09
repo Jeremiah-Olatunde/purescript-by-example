ec766eda09bcbfb042384c5765531afb9bd59968
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-09 21:44:04 +0100

    feat(ch8): add solution third

--- exercises/chapter8/test/MySolutions.purs
+++ exercises/chapter8/test/MySolutions.purs
@@ -2,4 +2,13 @@ module Test.MySolutions where
 
 import Prelude
 
+import Data.Maybe (Maybe)
+import Data.Array as Array
+
 -- Note to reader: Add your solutions to this file
+
+third :: forall a. Array a -> Maybe a
+third xs = do
+  from1 <- Array.tail xs
+  from2 <- Array.tail from1
+  Array.head from2
